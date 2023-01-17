d1069aab9e4b2968187d18479bf5651959995031
Universenn <dnwndhks98@gmail.com>
2023-01-17 17:00:50 +0900

    [Feat] Enum Role, Tier 생성

---
+++ backend/src/main/java/site/bookmore/bookmoreserver/users/entity/Role.java
@@ -0,0 +1,10 @@
+package site.bookmore.bookmoreserver.users.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    USER,ADMIN
+}
