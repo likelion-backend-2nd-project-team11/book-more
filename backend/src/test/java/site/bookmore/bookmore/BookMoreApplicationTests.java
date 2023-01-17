834688957db10e2be8cf3d5e81554564f06c706b
menuHwang <hmin0206@naver.com>
2023-01-17 22:09:23 +0900

    [Test] BookMoreApplicationTests 수정

--- backend/src/test/java/site/bookmore/bookmore/BookMoreApplicationTests.java
+++ backend/src/test/java/site/bookmore/bookmore/BookMoreApplicationTests.java
@@ -1,9 +1,7 @@
 package site.bookmore.bookmore;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookMoreApplicationTests {
 
     @Test
