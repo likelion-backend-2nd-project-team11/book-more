63dfbe278cb5bb69997676e7813d5717e709583a
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-01-18 15:11:38 +0900

    [Feat] 도서 리뷰 Entity 생성

---
+++ backend/src/main/java/site/bookmore/bookmore/books/entity/Review.java
@@ -0,0 +1,37 @@
+package site.bookmore.bookmore.books.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.bookmore.bookmore.common.entity.BaseEntity;
+import site.bookmore.bookmore.users.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reviews")
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Book book;
+
+    private String body;
+    private boolean spoiler;
+    private int professionalism;
+    private int fun;
+    private int readability;
+    private int collectible;
+    private int difficulty;
+    private int likesCount;
+}
