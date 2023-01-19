63981a37c4bab919d9dd75075f80dd9b95e5d22f
menuHwang <hmin0206@naver.com>
2023-01-19 22:50:13 +0900

    [Fix] Follow 수정

--- backend/src/main/java/site/bookmore/bookmore/users/repositroy/FollowRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/users/repositroy/FollowRepository.java
@@ -2,9 +2,10 @@ package site.bookmore.bookmore.users.repositroy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.bookmore.bookmore.users.entity.Follow;
+import site.bookmore.bookmore.users.entity.User;
 
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-    Optional<Follow> findByFollowerAndFollowing(Long follower, Long following);
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 }
