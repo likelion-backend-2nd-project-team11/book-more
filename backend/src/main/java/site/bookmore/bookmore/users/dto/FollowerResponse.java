97f533d9c1410f1fd19fcb1a4d2c3aca3bf9d2ef
yjyj1023 <lyj19961023@gmail.com>
2023-01-19 17:22:07 +0900

    [Refactor] FollowerResponse 수정
    ref: #19

--- backend/src/main/java/site/bookmore/bookmore/users/dto/FollowerResponse.java
+++ backend/src/main/java/site/bookmore/bookmore/users/dto/FollowerResponse.java
@@ -1,13 +1,27 @@
 package site.bookmore.bookmore.users.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.bookmore.bookmore.users.entity.Follow;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FollowerResponse {
     private Long id;
     private Long follower;
-    private LocalDateTime createdDatetime;
-    private LocalDateTime lastModifiedDatetime;
+    private String createdDatetime;
+    private String lastModifiedDatetime;
+
+    public FollowerResponse(Follow follow) {
+        this.id = follow.getId();
+        this.follower = follow.getFollower().getId();
+        this.createdDatetime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(follow.getCreatedDatetime());
+        this.lastModifiedDatetime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(follow.getLastModifiedDatetime());
+    }
 }
