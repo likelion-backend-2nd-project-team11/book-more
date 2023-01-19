1e8dc5658ce691638dc2482359e5583c3e624604
simhyunbo <hbo9347@naver.com>
2023-01-19 15:34:32 +0900

    [Update] Alarm Response
    ref : #10

--- backend/src/main/java/site/bookmore/bookmore/alarms/entity/dto/AlarmResponse.java
+++ backend/src/main/java/site/bookmore/bookmore/alarms/entity/dto/AlarmResponse.java
@@ -25,8 +25,8 @@ public class AlarmResponse {
     public AlarmResponse(Alarm alarm) {
         this.id = alarm.getId();
         this.alarmType = alarm.getAlarmType();
-        this.targetUser = alarm.getTargetUser();
-        this.fromUser = alarm.getFromUser();
+        this.targetUser = alarm.getTarget_User().getId();
+        this.fromUser = alarm.getFrom_User().getId();
         this.source = alarm.getSource();
         this.createdDatetime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(alarm.getCreatedDatetime());
     }
