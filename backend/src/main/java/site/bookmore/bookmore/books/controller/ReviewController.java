c340a302375c7db476279ea82e03aec209fae178
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-01-19 17:40:39 +0900

    [Update] 컨트롤러에 도서 리뷰 좋아요 기능 추가
    ref : #13

--- backend/src/main/java/site/bookmore/bookmore/books/controller/ReviewController.java
+++ backend/src/main/java/site/bookmore/bookmore/books/controller/ReviewController.java
@@ -14,10 +14,19 @@ import site.bookmore.bookmore.common.dto.ResultResponse;
 public class ReviewController {
     private final ReviewService reviewService;
 
+    // 도서 리뷰 등록
     @PostMapping("/{isbn}/reviews")
     public ResultResponse<ReviewResponse> create(@RequestBody ReviewRequest reviewRequest, @PathVariable String isbn, Authentication authentication) {
         String email = authentication.getName();
         Long id = reviewService.create(reviewRequest, isbn, email);
         return ResultResponse.success(new ReviewResponse(id, "리뷰 등록 완료"));
+    }
+
+    // 도서 리뷰에 좋아요 | 취소
+    @PostMapping("/reviews/{id}/likes")
+    public ResultResponse<String> likes(@PathVariable Long id, Authentication authentication) {
+        String email = authentication.getName();
+        boolean result = reviewService.doLikes(email, id);
+        return ResultResponse.success(result ? "좋아요를 눌렀습니다." : "좋아요가 취소되었습니다.");
     }
 }
