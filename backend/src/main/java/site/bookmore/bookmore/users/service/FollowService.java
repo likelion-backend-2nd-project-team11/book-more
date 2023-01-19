852ba438a334a29777ebadd0ea75918c1a34e6be
yjyj1023 <lyj19961023@gmail.com>
2023-01-19 10:35:13 +0900

    [Feat] FollowService 구현

---
+++ backend/src/main/java/site/bookmore/bookmore/users/service/FollowService.java
@@ -0,0 +1,71 @@
+package site.bookmore.bookmore.users.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.bookmore.bookmore.common.exception.bad_request.FollowNotMeException;
+import site.bookmore.bookmore.common.exception.not_found.FollowNotFoundException;
+import site.bookmore.bookmore.common.exception.not_found.UserNotFoundException;
+import site.bookmore.bookmore.users.entity.Follow;
+import site.bookmore.bookmore.users.entity.User;
+import site.bookmore.bookmore.users.repositroy.FollowRepository;
+import site.bookmore.bookmore.users.repositroy.UserRepository;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+    private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+
+    public String following(Long id, String email) {
+        //나
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        //팔로우할 유저
+        User targetUser = userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+
+        //자기 자신을 팔로우한 경우
+        if (Objects.equals(user.getId(), id)) {
+            throw new FollowNotMeException();
+        }
+
+        Follow follow = Follow.builder()
+                .following(targetUser)
+                .follower(user)
+                .build();
+
+        follow.undelete();
+
+        followRepository.save(follow);
+
+        return String.format("%s 님을 팔로우 하셨습니다.", id);
+    }
+
+    public String unfollowing(Long id, String email) {
+        //나
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        //팔로우할 유저
+        User targetUser = userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+
+        //팔로우 하지 않은 사람을 언팔로우 하는 경우
+        Follow targetFollow = followRepository.findByFollowerAndFollowing(user.getId(), targetUser.getId())
+                .orElseThrow(FollowNotFoundException::new);
+
+        Follow follow = Follow.builder()
+                .following(targetFollow.getFollowing())
+                .follower(targetFollow.getFollower())
+                .build();
+
+        follow.delete();
+
+        followRepository.save(follow);
+
+        return String.format("%s 님을 언팔로우 하셨습니다.", id);
+    }
+}
