c15d6c33226ef6ccbea085f1e0ed52c42c6878a7
Universenn <dnwndhks98@gmail.com>
2023-01-19 13:15:12 +0900

    [Feat] UserService login 구현
    ref : #12

--- backend/src/main/java/site/bookmore/bookmore/users/service/UserService.java
+++ backend/src/main/java/site/bookmore/bookmore/users/service/UserService.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Service;
 import site.bookmore.bookmore.common.exception.conflict.DuplicateEmailException;
 import site.bookmore.bookmore.common.exception.conflict.DuplicateNicknameException;
 import site.bookmore.bookmore.common.exception.not_found.UserNotFoundException;
+import site.bookmore.bookmore.common.exception.unauthorized.InvalidPasswordException;
 import site.bookmore.bookmore.security.provider.JwtProvider;
 import site.bookmore.bookmore.users.dto.UserJoinRequest;
 import site.bookmore.bookmore.users.dto.UserJoinResponse;
+import site.bookmore.bookmore.users.dto.UserLoginRequest;
+import site.bookmore.bookmore.users.dto.UserLoginResponse;
 import site.bookmore.bookmore.users.entity.User;
 import site.bookmore.bookmore.users.repositroy.UserRepository;
 
@@ -23,11 +26,6 @@ public class UserService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
-
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        return userRepository.findByEmail(username).orElseThrow(UserNotFoundException::new);
-//    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -48,6 +46,16 @@ public class UserService implements UserDetailsService {
         User user = userRepository.save(userJoinRequest.toEntity(encoded));
 
         return UserJoinResponse.of(user);
+    }
+
+    /**
+     * 로그인
+     */
+    public UserLoginResponse login(UserLoginRequest userLoginRequest) {
+        User user = userRepository.findByEmail(userLoginRequest.getEmail()).orElseThrow(UserNotFoundException::new);
+        if (!passwordEncoder.matches(userLoginRequest.getPassword(), user.getPassword()))
+            throw new InvalidPasswordException();
+        return new UserLoginResponse(jwtProvider.generateToken(user));
     }
 
 
