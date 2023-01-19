59c806766b4124b7e7d256205c3e7e9c0d889268
yjyj1023 <lyj19961023@gmail.com>
2023-01-19 16:07:56 +0900

    [Update] FollowController에 팔로워/팔로잉 조회 추가
    ref: #19

--- backend/src/main/java/site/bookmore/bookmore/users/controller/FollowController.java
+++ backend/src/main/java/site/bookmore/bookmore/users/controller/FollowController.java
@@ -1,9 +1,13 @@
 package site.bookmore.bookmore.users.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import site.bookmore.bookmore.common.dto.ResultResponse;
+import site.bookmore.bookmore.users.dto.FollowerResponse;
+import site.bookmore.bookmore.users.dto.FollowingResponse;
 import site.bookmore.bookmore.users.service.FollowService;
 
 import java.security.Principal;
@@ -25,5 +29,15 @@ public class FollowController {
     public ResultResponse<String> unfollowing(@PathVariable Long id, Authentication authentication) {
         String email = authentication.getName();
         return ResultResponse.success(followService.unfollowing(id, email));
+    }
+
+    @GetMapping("/{id}/following")
+    public ResultResponse<Page<FollowingResponse>> findAllFollowing(@PathVariable Long id, Pageable pageable) {
+        return ResultResponse.success(followService.findAllFollowing(id, pageable));
+    }
+
+    @GetMapping("/{id}/follower")
+    public ResultResponse<Page<FollowerResponse>> findAllFollower(@PathVariable Long id, Pageable pageable) {
+        return ResultResponse.success(followService.findAllFollower(id, pageable));
     }
 }
