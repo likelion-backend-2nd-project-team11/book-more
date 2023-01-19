d84ef8f80d83852f64b3c5affc498acb96a5b98f
yjyj1023 <lyj19961023@gmail.com>
2023-01-19 16:06:20 +0900

    [Update] Follow에 toResponse 추가
    ref: #19

--- backend/src/main/java/site/bookmore/bookmore/users/entity/Follow.java
+++ backend/src/main/java/site/bookmore/bookmore/users/entity/Follow.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import site.bookmore.bookmore.common.entity.BaseEntity;
+import site.bookmore.bookmore.users.dto.FollowerResponse;
+import site.bookmore.bookmore.users.dto.FollowingResponse;
 
 import javax.persistence.*;
 
@@ -25,4 +27,22 @@ public class Follow extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "follower",nullable = false)
     private User follower;
+
+    public FollowerResponse toFollowerResponse(){
+        return FollowerResponse.builder()
+                .id(this.id)
+                .follower(this.follower.getId())
+                .createdDatetime(this.getCreatedDatetime())
+                .lastModifiedDatetime(this.getLastModifiedDatetime())
+                .build();
+    }
+
+    public FollowingResponse toFollowingResponse(){
+        return FollowingResponse.builder()
+                .id(this.id)
+                .following(this.following.getId())
+                .createdDatetime(this.getCreatedDatetime())
+                .lastModifiedDatetime(this.getLastModifiedDatetime())
+                .build();
+    }
 }
