cb59f632af4e956196181d66a41595584ddde7fb
yjyj1023 <lyj19961023@gmail.com>
2023-01-20 02:09:26 +0900

    [Update] FollowRepository JPA 추가
    ref: #28

--- backend/src/main/java/site/bookmore/bookmore/users/repositroy/FollowRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/users/repositroy/FollowRepository.java
@@ -1,5 +1,7 @@
 package site.bookmore.bookmore.users.repositroy;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.bookmore.bookmore.users.entity.Follow;
 import site.bookmore.bookmore.users.entity.User;
@@ -8,4 +10,10 @@ import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByFollowerAndFollowing(User follower, User following);
+
+    Optional<Follow> findByFollowerAndFollowingAndDeletedDatetimeIsNull(User follower, User following);
+
+    Page<Follow> findByFollowerAndDeletedDatetimeIsNull(Pageable pageable, User follower);
+
+    Page<Follow> findByFollowingAndDeletedDatetimeIsNull(Pageable pageable, User following);
 }
