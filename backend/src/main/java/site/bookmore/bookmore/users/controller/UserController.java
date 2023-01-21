5d14dbaf1df86c635755cfbefddd3a475b1cd6df
Universenn <dnwndhks98@gmail.com>
2023-01-21 18:07:08 +0900

    [Feat] 회원 정보 수정 controller 생성
    ref : #34

--- backend/src/main/java/site/bookmore/bookmore/users/controller/UserController.java
+++ backend/src/main/java/site/bookmore/bookmore/users/controller/UserController.java
@@ -1,13 +1,13 @@
 package site.bookmore.bookmore.users.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import site.bookmore.bookmore.common.dto.ResultResponse;
 import site.bookmore.bookmore.users.dto.*;
 import site.bookmore.bookmore.users.service.UserService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,5 +24,11 @@ public class UserController {
     @PostMapping("/login")
     public ResultResponse<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
         return ResultResponse.success(userService.login(userLoginRequest));
+    }
+
+    @PatchMapping("/{id}")
+    public ResultResponse<UserUpdateResponse> update(@RequestBody UserUpdateRequest userUpdateRequest, @PathVariable Long id, Authentication authentication) {
+        String email = authentication.getName();
+        return ResultResponse.success(userService.infoUpdate(email, id, userUpdateRequest));
     }
 }
