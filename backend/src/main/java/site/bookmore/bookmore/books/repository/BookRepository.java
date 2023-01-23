207fdfa17ddc47d610fe4a7af2e8c49ea37d086f
menuHwang <hmin0206@naver.com>
2023-01-23 17:13:59 +0900

    [Refactor] 도서 상세조회 JPA N+1 문제 튜닝
    ref : #8

--- backend/src/main/java/site/bookmore/bookmore/books/repository/BookRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/books/repository/BookRepository.java
@@ -1,7 +1,13 @@
 package site.bookmore.bookmore.books.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.bookmore.bookmore.books.entity.Book;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, String> {
+    @Query("SELECT b FROM Book b JOIN FETCH b.authors JOIN FETCH  b.translators WHERE b.id=:isbn")
+    Optional<Book> findById(@Param("isbn") String isbn);
 }
