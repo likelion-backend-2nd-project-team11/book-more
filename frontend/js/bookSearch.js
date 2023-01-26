06a32250646290f4882093b60e5bc3fa9d30e684
menuHwang <hmin0206@naver.com>
2023-01-26 11:30:47 +0900

    [Feat] 도서 검색 api 연결
    ref : #68

---
+++ frontend/js/bookSearch.js
@@ -0,0 +1,28 @@
+const urlSearchParams = new URLSearchParams(location.search);
+const input = document.querySelector('.search-box input');
+const query = urlSearchParams.get('query');
+const page = urlSearchParams.get('page');
+const size = urlSearchParams.get('size');
+const BASE_URL = 'http://localhost:8080';
+
+function search(query, page, size) {
+    if (query.length > 1) window.location.href=`./books.html?query=${query}&page=${page | 1}&size=${size | 20}`;
+    else alert('검색어를 확인해주세요. [최소 두 글자]');
+}
+function getQuery() {
+    return input.value;
+}
+
+function setQueryInputVal(value) {
+    input.value = value;
+}
+
+function fetchSearchBooks(query, page, size) {
+    fetch(`${BASE_URL}/api/v1/books?${query}&page=1&size=20`)
+        .then(res => {
+            if (res.ok) return res.json();
+            else alert(res.json()['result']['message']);
+        }).then(data => {
+            console.log(data);
+    })
+}
