c74f65d2e8fda311be2845e44beabcf704fd9d51
menuHwang <hmin0206@naver.com>
2023-01-26 15:18:57 +0900

    [Feat] 도서 검색 api 연동
    ref : #68

--- frontend/js/bookSearch.js
+++ frontend/js/bookSearch.js
@@ -3,6 +3,7 @@ const input = document.querySelector('.search-box input');
 const query = urlSearchParams.get('query');
 const page = urlSearchParams.get('page');
 const size = urlSearchParams.get('size');
+const isbn = urlSearchParams.get('isbn');
 const BASE_URL = 'http://localhost:8080';
 
 function search(query, page, size) {
@@ -18,12 +19,65 @@ function setQueryInputVal(value) {
 }
 
 function fetchSearchBooks(query, page, size) {
-    if (query === '' || query === undefined) return;
-    fetch(`${BASE_URL}/api/v1/books?query=${query}&page=1&size=20`)
+    if (query == '' || query == undefined) return;
+    fetch(`${BASE_URL}/api/v1/books?query=${query}&page=${page}&size=${size}`)
         .then(res => {
             if (res.ok) return res.json();
-            else alert(res.json()['result']['message']);
+            else alert(res.json().result.message);
         }).then(data => {
-            console.log(data);
+            console.log(data)
+            const books = data.result.content;
+            const contentWrapper = document.querySelector('.contents-wrapper');
+            contentWrapper.innerHTML = books.map(book => {
+                return `
+                <a class="text-decoration-none text-black" onclick="window.location.href='./book.html?isbn=${book.isbn}'">
+                    <div class="book-item">
+                        <img width="130px" src="${book.image}"/>
+                        <div class="info w-100">
+                            <h2>${book.title}</h2>
+                            <hr>
+                            <p>저자 : ${book.authors}</p>
+                            <p>가격 : ${book.price}</p>
+                            <p>출판사 : ${book.publisher}</p>
+                        </div>
+                    </div>
+                </a class="text-decoration-none text-black">`;
+            }).join('\n');
+    })
+}
+
+function fetchSearchBookDetail(isbn) {
+    if (isbn == '' || isbn == undefined) return;
+    fetch(`${BASE_URL}/api/v1/books/${isbn}`)
+        .then(res => {
+            if (res.ok) return res.json();
+            else alert(res.json().result.message);
+        }).then(data => {
+        console.log(data)
+        const book = data.result;
+        const contentWrapper = document.querySelector('.contents-wrapper');
+        contentWrapper.innerHTML = `
+                <div class="container contents p-5 shadow bg-white">
+                    <div class="book-detail d-flex">
+                        <img class="me-5 shadow" height="240px" src="${book.image}"/>
+                        <div class="info">
+                            <h2>${book.title}</h2>
+                            <p>가격 : ${book.price}원</p>
+                            <p>페이지 : ${book.pages}</p>
+                            <p>저자 : ${book.authors}</p>
+                            <p>번역 : ${book.translators}</p>
+                            <p>출판사 : ${book.publisher}</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div>
+                        <h4>책소개</h4>
+                        <p>${book.introduce}...</p>
+                        <h4>목차</h4>
+                        <p class="desc">
+                            ${book.chapter}
+                        </p>
+                    </div>
+                </div>`;
     })
 }
