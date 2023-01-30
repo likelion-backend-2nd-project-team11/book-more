e7e90169336fd06fad12b8a8367c465181449654
simhyunbo <hbo9347@naver.com>
2023-01-30 16:56:17 +0900

    [Update] Ranks
    Ref : #42

--- backend/src/main/java/site/bookmore/bookmore/users/entity/Ranks.java
+++ backend/src/main/java/site/bookmore/bookmore/users/entity/Ranks.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Builder
