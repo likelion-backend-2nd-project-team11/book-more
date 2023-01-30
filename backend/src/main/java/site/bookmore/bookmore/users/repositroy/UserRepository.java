2e09402285c37b6504ec71797353f1e63a7d01a4
simhyunbo <hbo9347@naver.com>
2023-01-30 11:15:52 +0900

    [Update] Review , User Repository 변경
    Ref : #42

--- backend/src/main/java/site/bookmore/bookmore/users/repositroy/UserRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/users/repositroy/UserRepository.java
@@ -1,5 +1,7 @@
 package site.bookmore.bookmore.users.repositroy;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.bookmore.bookmore.users.entity.User;
 
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByNickname(String nickname);
+
+    Page<User> findAll(Pageable pageable);
 }
