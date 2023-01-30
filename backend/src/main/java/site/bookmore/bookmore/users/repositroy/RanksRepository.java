c1d2d46418d71fc62caf6820e4f48adc879ecb1b
simhyunbo <hbo9347@naver.com>
2023-01-30 16:56:57 +0900

    [Update] RanksRepository 변경
    Ref : #42

--- backend/src/main/java/site/bookmore/bookmore/users/repositroy/RanksRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/users/repositroy/RanksRepository.java
@@ -1,7 +1,5 @@
 package site.bookmore.bookmore.users.repositroy;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import site.bookmore.bookmore.users.entity.Ranks;
@@ -11,11 +9,9 @@ import java.util.List;
 
 public interface RanksRepository extends JpaRepository<Ranks, Long> {
 
-
-    @Query(value = "SELECT r.id,r.point, (Rank() over(order by r.point desc)) as ranking FROM ranks r", nativeQuery = true)
-    Page<Ranks> findAllRanks(Pageable pageable);
-
     @Query(value = "SELECT r.id,r.point, (Rank() over(order by r.point desc)) as ranking FROM ranks r", nativeQuery = true)
     List<RanksNativeVo> findAllRanking();
+
+    List<Ranks> findTop100ByOrderByRankingAsc();
 
 }
