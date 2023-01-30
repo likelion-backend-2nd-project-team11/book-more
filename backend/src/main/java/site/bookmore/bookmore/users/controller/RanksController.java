5c80254159f6d960e32fead748bd296107b88b46
simhyunbo <hbo9347@naver.com>
2023-01-30 16:56:37 +0900

    [Update] RanksController 변경
    Ref : #42

--- backend/src/main/java/site/bookmore/bookmore/users/controller/RanksController.java
+++ backend/src/main/java/site/bookmore/bookmore/users/controller/RanksController.java
@@ -1,15 +1,14 @@
 package site.bookmore.bookmore.users.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.bookmore.bookmore.common.dto.ResultResponse;
 import site.bookmore.bookmore.users.dto.RanksResponse;
 import site.bookmore.bookmore.users.service.RanksService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +17,9 @@ public class RanksController {
     private final RanksService ranksService;
 
     @GetMapping("/ranks")
-    public ResultResponse<Page<RanksResponse>> findRanks(Pageable pageable, Authentication authentication) {
+    public ResultResponse<List<RanksResponse>> findTop100Ranks() {
 
-        return ResultResponse.success(ranksService.findRanks(pageable, authentication.getName()));
+        return ResultResponse.success(ranksService.findTop100Ranks());
     }
 
 }
