303c4fdb1ef91d0e72c3e53ff552a8b0c604a96c
menuHwang <hmin0206@naver.com>
2023-01-30 20:09:49 +0900

    [Test] Book Service, Controller
    - 네이버 api 연동으로 인한 리팩터링
    - DTO, Param 수정으로 인한 리팩터링
    
    ref : #85

--- backend/src/test/java/site/bookmore/bookmore/books/controller/BookControllerTest.java
+++ backend/src/test/java/site/bookmore/bookmore/books/controller/BookControllerTest.java
@@ -9,17 +9,14 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import site.bookmore.bookmore.books.dto.BookDetailResponse;
 import site.bookmore.bookmore.books.dto.BookResponse;
+import site.bookmore.bookmore.books.dto.BookSearchParams;
+import site.bookmore.bookmore.books.entity.Subject;
 import site.bookmore.bookmore.books.service.BookService;
-import site.bookmore.bookmore.books.util.api.kakao.dto.Document;
-import site.bookmore.bookmore.books.util.api.kakao.dto.KakaoSearchParams;
-import site.bookmore.bookmore.books.util.api.kakao.dto.KakaoSearchResponse;
-import site.bookmore.bookmore.books.util.api.kakao.dto.Meta;
-import site.bookmore.bookmore.books.util.mapper.BookResponseMapper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -46,57 +43,33 @@ class BookControllerTest {
     void search() throws Exception {
         //given
         int SIZE = 10;
-        List<Document> documents = new ArrayList<>();
+        List<BookResponse> bookResponses = new ArrayList<>();
         for (int i = 0; i < SIZE; i++) {
-            Document ele = Document.builder()
+            BookResponse ele = BookResponse.builder()
                     .title("title" + i)
-                    .authors(List.of("authors"))
-                    .translators(List.of("translator"))
-                    .contents("contents" + 1)
                     .isbn(String.valueOf(i))
-                    .price(10000)
-                    .url("http://test.com/" + i)
+                    .image("http://test.com/" + i)
                     .build();
-            documents.add(ele);
+            bookResponses.add(ele);
         }
-        Meta meta = Meta.builder()
-                .pageable_count(SIZE)
-                .total_count(SIZE)
-                .is_end(false)
-                .build();
 
-        KakaoSearchResponse kakaoSearchResponse = new KakaoSearchResponse(meta, documents);
-        List<BookResponse> bookResponses = kakaoSearchResponse.getDocuments().stream().map(BookResponseMapper::of).collect(Collectors.toList());
-
-        given(bookService.search(any(KakaoSearchParams.class))).willReturn(new PageImpl<>(bookResponses, Pageable.unpaged(),kakaoSearchResponse.getMeta().getPageable_count()));
+        given(bookService.search(any(BookSearchParams.class))).willReturn(new PageImpl<>(bookResponses, Pageable.unpaged(), SIZE));
 
         mockMvc.perform(get("/api/v1/books?query=정의란 무엇인가")
                         .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.resultCode").value(SUCCESS))
                 .andExpect(jsonPath("$.result.content").isArray())
-                .andExpect(jsonPath("$.result.totalElements").value(meta.getPageable_count()));
+                .andExpect(jsonPath("$.result.totalElements").exists());
 
-        verify(bookService).search(any(KakaoSearchParams.class));
+        verify(bookService).search(any(BookSearchParams.class));
     }
 
     @Test
     @DisplayName("도서 리스트 검색 - 검색 결과 없음")
     @WithMockUser
     void search_documents_empty() throws Exception {
-        //given
-        List<Document> documents = new ArrayList<>();
-        Meta meta = Meta.builder()
-                .pageable_count(0)
-                .total_count(0)
-                .is_end(true)
-                .build();
-
-        KakaoSearchResponse kakaoSearchResponse = new KakaoSearchResponse(meta, documents);
-
-        List<BookResponse> bookResponses = kakaoSearchResponse.getDocuments().stream().map(BookResponseMapper::of).collect(Collectors.toList());
-
-        given(bookService.search(any(KakaoSearchParams.class))).willReturn(new PageImpl<>(bookResponses));
+        given(bookService.search(any(BookSearchParams.class))).willReturn(new PageImpl<>(new ArrayList<>()));
 
         mockMvc.perform(get("/api/v1/books?query=책이름")
                         .with(csrf()))
@@ -104,8 +77,42 @@ class BookControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.resultCode").value(SUCCESS))
                 .andExpect(jsonPath("$.result.content").isArray())
-                .andExpect(jsonPath("$.result.totalElements").value(meta.getPageable_count()));
+                .andExpect(jsonPath("$.result.totalElements").exists());
 
-        verify(bookService).search(any(KakaoSearchParams.class));
+        verify(bookService).search(any(BookSearchParams.class));
+    }
+
+    @Test
+    @DisplayName("도서 상세 검색 정상")
+    @WithMockUser
+    void searchByISBN() throws Exception {
+        BookDetailResponse bookDetailResponse = BookDetailResponse.builder()
+                                                                    .isbn("10001")
+                                                                    .title("title")
+                                                                    .subject(Subject.예술)
+                                                                    .publisher("publisher")
+                                                                    .pages(100)
+                                                                    .image("http://test.com/img/111.jpg")
+                                                                    .introduce("intro")
+                                                                    .price(10000)
+                                                                    .build();
+
+        given(bookService.searchByISBN("10001")).willReturn(bookDetailResponse);
+
+        mockMvc.perform(get("/api/v1/books/10001")
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value(SUCCESS))
+                .andExpect(jsonPath("$.result.isbn").value(bookDetailResponse.getIsbn()))
+                .andExpect(jsonPath("$.result.title").value(bookDetailResponse.getTitle()))
+                .andExpect(jsonPath("$.result.subject").value(bookDetailResponse.getSubject().name()))
+                .andExpect(jsonPath("$.result.publisher").value(bookDetailResponse.getPublisher()))
+                .andExpect(jsonPath("$.result.pages").value(bookDetailResponse.getPages()))
+                .andExpect(jsonPath("$.result.image").value(bookDetailResponse.getImage()))
+                .andExpect(jsonPath("$.result.introduce").value(bookDetailResponse.getIntroduce()))
+                .andExpect(jsonPath("$.result.price").value(bookDetailResponse.getPrice()));
+
+        verify(bookService).searchByISBN("10001");
     }
 }
