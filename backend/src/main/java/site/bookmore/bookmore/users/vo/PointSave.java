79d99b6a4bbd46f6d2ae7552712d26637c49b84e
simhyunbo <hbo9347@naver.com>
2023-01-30 11:13:53 +0900

    [Feat] Ranks Vo
    Ref : #42

---
+++ backend/src/main/java/site/bookmore/bookmore/users/vo/PointSave.java
@@ -0,0 +1,11 @@
+package site.bookmore.bookmore.users.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PointSave {
+    private Long Id;
+    private Integer point;
+}
