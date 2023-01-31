ba6632809b3ed01fd2ec01997f1af913bb954a59
menuHwang <hmin0206@naver.com>
2023-01-31 19:54:48 +0900

    [Feat] ranking controller 스웨거 설정
    ref : #114

--- backend/src/main/java/site/bookmore/bookmore/users/controller/RanksController.java
+++ backend/src/main/java/site/bookmore/bookmore/users/controller/RanksController.java
@@ -1,5 +1,7 @@
 package site.bookmore.bookmore.users.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "7-랭킹")
 @RequestMapping("/api/v1/users")
 public class RanksController {
     private final RanksService ranksService;
 
+    @ApiOperation(value = "랭킹 Top100 조회")
     @GetMapping("/ranks")
     public ResultResponse<List<RanksResponse>> findTop100Ranks() {
 
