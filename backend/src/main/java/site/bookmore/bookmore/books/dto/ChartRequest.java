8af956c01b194570996a55cf919c1acf4f1f86a5
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-02-02 13:45:52 +0900

    [Feature] ReviewRequest 안의 Chart -> ChartRequest로 분리
    ref : #137

---
+++ backend/src/main/java/site/bookmore/bookmore/books/dto/ChartRequest.java
@@ -0,0 +1,42 @@
+package site.bookmore.bookmore.books.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.bookmore.bookmore.books.entity.Chart;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class ChartRequest {
+    @Min(value = 1, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    @Max(value = 5, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    private Integer professionalism;
+    @Min(value = 1, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    @Max(value = 5, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    private Integer fun;
+    @Min(value = 1, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    @Max(value = 5, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    private Integer readability;
+    @Min(value = 1, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    @Max(value = 5, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    private Integer collectible;
+    @Min(value = 1, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    @Max(value = 5, message = "1 ~ 5점 사이의 점수만 부여할 수 있습니다.")
+    private Integer difficulty;
+
+    public Chart toEntity() {
+        return Chart.builder()
+                .professionalism(professionalism)
+                .fun(fun)
+                .readability(readability)
+                .collectible(collectible)
+                .difficulty(difficulty)
+                .build();
+    }
+}
