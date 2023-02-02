db54257f6a207db4e781ecd8c20c9fc62eda3fac
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-02-02 13:47:01 +0900

    [Update] 에러 메시지 맞추기 위해 exception 수정
    ref : #137

--- backend/src/main/java/site/bookmore/bookmore/common/dto/ErrorResponse.java
+++ backend/src/main/java/site/bookmore/bookmore/common/dto/ErrorResponse.java
@@ -3,6 +3,8 @@ package site.bookmore.bookmore.common.dto;
 import lombok.Getter;
 import site.bookmore.bookmore.common.exception.ErrorCode;
 
+import java.util.Map;
+
 @Getter
 public class ErrorResponse {
     private final String errorCode;
@@ -13,7 +15,23 @@ public class ErrorResponse {
         this.message = errorCode.getMessage();
     }
 
+    public ErrorResponse(Map<String, String> result) {
+
+        String key = "";
+
+        for (String keys : result.keySet()) {
+            key = keys;
+        }
+
+        this.errorCode = key + " 입력값 오류";
+        this.message = result.get(key);
+    }
+
     public static ErrorResponse of(ErrorCode errorCode) {
         return new ErrorResponse(errorCode);
+    }
+
+    public static ErrorResponse of(Map<String, String> result) {
+        return new ErrorResponse(result);
     }
 }
