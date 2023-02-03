2be177a8aecc8ace64c64a4e2e96304df31581f4
simhyunbo <hbo9347@naver.com>
2023-02-03 09:20:38 +0900

    [Feat] Alarm 이벤트 핸들러
    Ref : #138

--- backend/src/main/java/site/bookmore/bookmore/observer/handler/AlarmEventHandler.java
+++ backend/src/main/java/site/bookmore/bookmore/observer/handler/AlarmEventHandler.java
@@ -5,8 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import site.bookmore.bookmore.alarms.entity.Alarm;
+import site.bookmore.bookmore.alarms.entity.AlarmType;
 import site.bookmore.bookmore.alarms.repository.AlarmRepository;
 import site.bookmore.bookmore.observer.event.alarm.AlarmCreate;
+import site.bookmore.bookmore.observer.event.alarm.AlarmListCreate;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +23,22 @@ public class AlarmEventHandler {
     public void createAlarm(AlarmCreate e) {
         log.info("알람 생성 type:{}, from:{}, target:{}, source:{}", e.getAlarm().getAlarmType(), e.getAlarm().getFromUser(), e.getAlarm().getTargetUser(), e.getAlarm().getSource());
         alarmRepository.save(e.getAlarm());
+        log.info("알람 생성 완료.");
+    }
+
+    @Async
+    @EventListener
+    public void createAlarmList(AlarmListCreate e) {
+        for (int i = 0; i < e.getTargetUsers().size(); i++) {
+            Alarm alarm = Alarm.builder()
+                    .alarmType(AlarmType.NEW_FOLLOW_REVIEW)
+                    .targetUser(e.getTargetUsers().get(i))
+                    .fromUser(e.getFromUser())
+                    .source(e.getSource())
+                    .build();
+            alarmRepository.save(alarm);
+        }
+
         log.info("알람 생성 완료.");
     }
 }
