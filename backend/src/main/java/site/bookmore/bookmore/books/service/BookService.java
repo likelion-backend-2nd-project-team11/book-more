a90398f04ea0839746e1a4cca82b3bf590401814
yjyj1023 <lyj19961023@gmail.com>
2023-02-07 10:56:20 +0900

    [Feat] 크롤링 추가

--- backend/src/main/java/site/bookmore/bookmore/books/service/BookService.java
+++ backend/src/main/java/site/bookmore/bookmore/books/service/BookService.java
@@ -2,6 +2,10 @@ package site.bookmore.bookmore.books.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +24,7 @@ import site.bookmore.bookmore.books.util.api.naver.NaverBooksearch;
 import site.bookmore.bookmore.books.util.api.naver.dto.NaverSearchParams;
 import site.bookmore.bookmore.common.exception.not_found.BookNotFoundException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -75,5 +80,25 @@ public class BookService {
         timer.stop();
         log.info("총 응답 시간 : {}ms", timer.getTotalTimeMillis());
         return BookDetailResponse.of(book);
+    }
+
+    public static void crawler(String url) {
+        //String url = "https://search.shopping.naver.com/book/catalog/32478261622";
+        try {
+            Connection conn = Jsoup.connect(url);
+
+            Document html = conn.get();
+
+            Elements files = html.select(".bookBasicInfo_info_detail__I0Fx5 > span:nth-child(1)");
+            String page = files.text();
+            System.out.println(page);
+
+            Elements files2 = html.select("#book_section-info > div:nth-child(5) > div.infoItem_data_box__VwBf3 > div.infoItem_data_text__bUgVI");
+            String content = files2.html();
+            System.out.println(content);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
