e188bd64327a8acca7f5bde356c6d779b5b38a7e
simhyunbo <hbo9347@naver.com>
2023-02-10 02:56:53 +0900

    [Feat] id = passwordCheck으로 변경
    Ref : #211

--- frontend/js/join.js
+++ frontend/js/join.js
@@ -9,7 +9,7 @@ function join() {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const passwordCheck = document.getElementById('password2').value;
+    const passwordCheck = document.getElementById('passwordCheck').value;
     const nickname = document.getElementById('name').value;
     const birth = birthArr.join("-");
 
