4aaf2c8ce9e25231473c91d076aa9d31ff31a328
menuHwang <hmin0206@naver.com>
2023-02-09 17:23:28 +0900

    [Fix] 내 정보 수정 생일 셀렉트로 변경
    ref : #193

--- frontend/js/birth.js
+++ frontend/js/birth.js
@@ -1,17 +1,18 @@
-let today = new Date();
-let today_year= today.getFullYear();
-let start_year=today_year-100;// 시작할 년도
-let index=0;
-for(let y=today_year; y>=start_year; y--){ // 현재 년도 ~ start_year
-    document.getElementById('select_year').options[index] = new Option(y, y);
-    index++;
+function birthSelectInit() {
+    let today = new Date();
+    let today_year= today.getFullYear();
+    let start_year=today_year-100;// 시작할 년도
+    let index=0;
+    for(let y=today_year; y>=start_year; y--){ // 현재 년도 ~ start_year
+        document.getElementById('select_year').options[index] = new Option(y, y);
+        index++;
+    }
+    index=0;
+    for(let m=1; m<=12; m++){
+        document.getElementById('select_month').options[index] = new Option(m, m);
+        index++;
+    }
 }
-index=0;
-for(let m=1; m<=12; m++){
-    document.getElementById('select_month').options[index] = new Option(m, m);
-    index++;
-}
-
 
 function lastday(){ //년과 월에 따라 마지막 일 구하기
     let Year=document.getElementById('select_year').value;
@@ -29,6 +30,47 @@ function lastday(){ //년과 월에 따라 마지막 일 구하기
         for(let i=dayindex_len; i>=day; i--){
             document.getElementById('select_day').options[i]=null;
         }
+    }
+}
+
+function setMyBirth(birth){ //년과 월에 따라 마지막 일 구하기
+    let tempBirth = birth.split('-');
+    birthArr[0] = tempBirth[0];
+    birthArr[1] = tempBirth[1];
+    birthArr[2] = tempBirth[2];
+
+    let Year = parseInt(tempBirth[0], 10);
+    let Month = parseInt(tempBirth[1], 10);
+    let Day = parseInt(tempBirth[2], 10);
+    let date=new Date(new Date(Year,Month,1)-86400000).getDate();
+    /* = new Date(new Date(Year,Month,0)).getDate(); */
+
+    const selectYear = document.getElementById('select_year');
+    const selectMonth = document.getElementById('select_month');
+    const selectDay = document.getElementById('select_day');
+
+    let dayindex_len=selectDay.length;
+    if(date>dayindex_len){
+        for(let i=(dayindex_len+1); i<=date; i++){
+            selectDay.options[i-1] = new Option(i, i);
+        }
+    }
+    else if(date<dayindex_len){
+        for(let i=dayindex_len; i>=date; i--){
+            document.getElementById('select_day').options[i]=null;
+        }
+    }
+
+    for (let i = 0; i <selectYear.length; i++) {
+        if (selectYear.options[i].value === tempBirth[0]) selectYear.options[i].selected = true;
+    }
+
+    for (let i = 0; i <selectMonth.length; i++) {
+        if (selectMonth.options[i].value === Month.toString()) selectMonth.options[i].selected = true;
+    }
+
+    for (let i = 0; i < selectDay.length; i++) {
+        if (selectDay.options[i].value === Day.toString()) selectDay.options[i].selected = true;
     }
 }
 
--- frontend/js/mypage.js
+++ frontend/js/mypage.js
@@ -5,7 +5,7 @@
  *  4. null 값 에러
  */
 function getMyPage(token) {
-    fetch(`${BASE_URL}/api/v1/users/me`, {
+    return fetch(`${BASE_URL}/api/v1/users/me`, {
         method: 'GET',
         headers: {
             "Authorization": token ? "Bearer " + token : '',
@@ -20,21 +20,7 @@ function getMyPage(token) {
                         <p>프로필 사진</p>
                         <div>
                         <button type="button" data-bs-toggle="modal" data-bs-target="#myprofileModal" style="border: none; border-radius: 0.7em; position: absolute; bottom: 0;margin-left: 35px;">Edit</button>
-                        </div>
-                        
-                        
-<!--                         <div class="form-floating mb-3 text-center">-->
-<!--                                            <input type="file" id="update-image" name="avatar" accept="image/png, image/jpeg">-->
-<!--                                            <button class="btn btn-primary w-70" onclick="updateImage(token)">프로필 변경</button>-->
-<!--                                        </div>-->
-
-<!--                                       <div class="filebox">-->
-<!--                                            <input id="update-image" name="avatar" accept="image/png, image/jpeg" class="upload-name" style="display: inline-block;height: 40px;padding: 0 10px;vertical-align: middle;border:1px solid #dddddd; width: 62%; color: #999999;float: right;" value="첨부파일" placeholder="첨부파일">-->
-<!--                                            <label style="display:inline-block;padding: 10px 20px; color: #fff;vertical-align: middle; background-color: #999999;cursor: pointer;height: 40px;margin-left: 10px;" for="file">파일찾기</label> -->
-<!--                                            <input style="position: absolute; width: 0;height: 0;padding: 0;overflow: hidden;border: 0;" type="file" id="file">-->
-<!--                                            <button class="w-70" style="float: right; border: none; border-radius: 0.7em;" onclick="updateImage(token)">프로필 변경</button>-->
-<!--                                        </div>-->
-                        
+                        </div>                        
                         <img style="margin: 0 auto;" class="rounded-circle" src="https://www.bookmore.site/${data.result.profile}" width="150px" height="150px">
                     </div>
                     <hr>
@@ -46,71 +32,10 @@ function getMyPage(token) {
                     <p>생년월일 : ${data.result.birth}</p>
 
                     <button class ="bm-button" type="button" onclick=deleteUser(token)>회원 탈퇴</button>
-                </div>
-                
-                    <div class="modal fade" id="modifyModal" tabindex="-1" aria-labelledby="modifyModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5 " id="modifyModalLabel">프로필 변경</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row m-4">
-                                <div class="input-form col-md-12 mx-auto">
-                                    <div>
-        
- 
-        
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" id="update-nickname" value="${data.result.nickname}" placeholder="제목을 입력하세요." required>
-                                            <label for="update-nickname">닉네임</label>
-                                        </div>
-        
-                                        <div class="form-floating mb-3">
-                                            <input type="password" class="form-control"  id="update-password" placeholder="제목을 입력하세요." required>
-                                            <label for="update-password">비밀번호</label>
-                                        </div>
-        
-<!--                                        <div class="form-floating mb-3">-->
-<!--                                            <input type="password" class="form-control"  id="pass check" placeholder="제목을 입력하세요." required>-->
-<!--                                            <label for="update-password">비밀번호 확인</label>-->
-<!--                                        </div>-->
-        
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" id="update-birth" value="${data.result.birth}" placeholder="1900-01-01" required>
-                                            <label for="update-birth">생년월일 </label>
-                                        </div>
-        
-        
-                                        <div class="mt-3">
-                                            <button class="btn btn-primary w-100" onclick="editUser(token)">수정</button>
-        <!--                                    <button class="btn btn-primary w-100" onclick="editUser(token), updateImage(token)">수정</button>-->
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-`;
+                </div>`;
+            return data;
         });
-};
-
-// $("#file").on('change',function(){
-//     var fileName = $("#file").val();
-//     $(".upload-name").val(fileName);
-// });
-
-
-
-
-
-
-
-
+}
 
 function deleteUser(token) {
     fetch(`${BASE_URL}/api/v1/users/me`, {
@@ -135,7 +60,7 @@ function deleteUser(token) {
 function editUser(token) {
     const nickname = document.getElementById("update-nickname").value;
     const password = document.getElementById("update-password").value;
-    const birth = document.getElementById("update-birth").value;
+    const birth = birthArr.join("-");
 
     const data = {
         nickname,
