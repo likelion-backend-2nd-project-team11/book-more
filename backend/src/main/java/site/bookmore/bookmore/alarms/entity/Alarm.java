90cb7c284c2467afd7d9721f5a5ffd449e5a9260
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-02-10 15:20:05 +0900

    [Update] 삭제 컬럼이 필요하여 BaseEntity를 상속받도록 수정
    ref : #223

--- backend/src/main/java/site/bookmore/bookmore/alarms/entity/Alarm.java
+++ backend/src/main/java/site/bookmore/bookmore/alarms/entity/Alarm.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import site.bookmore.bookmore.common.entity.BaseEntity;
 import site.bookmore.bookmore.users.entity.User;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "alarm")
@@ -18,7 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Alarm {
+public class Alarm extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,10 +31,6 @@ public class Alarm {
 
     @Column(nullable = false)
     private boolean confirmed;
-
-    @CreatedDate
-    @Column(name = "created_datetime")
-    private LocalDateTime createdDatetime;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "target_user", nullable = false, foreignKey = @ForeignKey(name = "fk_alarm_target_user"))
