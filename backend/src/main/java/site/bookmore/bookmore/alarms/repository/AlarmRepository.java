242a83a4f9ca914170a0ebf4ae2f36d253c0cfb7
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-02-10 15:21:38 +0900

    [Update] 기존 메서드: 삭제된 알림은 제외하고 가져오도록 수정 & 탈퇴한 유저가 받은 알림을 리스트로 가져오는 메서드 생성
    ref : #223

--- backend/src/main/java/site/bookmore/bookmore/alarms/repository/AlarmRepository.java
+++ backend/src/main/java/site/bookmore/bookmore/alarms/repository/AlarmRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.bookmore.bookmore.alarms.entity.Alarm;
 import site.bookmore.bookmore.users.entity.User;
 
+import java.util.List;
+
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-    Page<Alarm> findByTargetUser(User target, Pageable pageable);
-    Page<Alarm> findByTargetUserAndConfirmedIsFalse(User target, Pageable pageable);
+    Page<Alarm> findByTargetUserAndDeletedDatetimeIsNull(User target, Pageable pageable);
+
+    Page<Alarm> findByTargetUserAndConfirmedIsFalseAndDeletedDatetimeIsNull(User target, Pageable pageable);
+
+    List<Alarm> findByTargetUser(User fromUser);
 }
